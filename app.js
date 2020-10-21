e4ff3db8e77f978f934ed32798436abba7e83ed4
Carla <carla@layc-dc.org>
2020-10-21 15:21:30 -0400

    added function for manager and engineer questions along with variable to request new team members

--- app.js
+++ app.js
@@ -10,21 +10,80 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
+myTeam = [];
 
 //make a function to use inquirer to get info on your manager
-//once our have the info, you will make a new manager with user.answers weith your manager class
+//Then once I have the info, make a new manager with user.answers with your manager class
 function createManager(){
-    console.log("Please put in your manager info...");
+    console.log("Please type in your manager's information...");
     inquirer.prompt([
         {
             type: "input",
             name: "managerName",
-            message: "What is the name of your manager?"
-        },{
+            message: "What is your manager's name?",
+        },
+        {
             type: "input",
-            name: "managerName",
-            message: "What is the name of your manager?"
+            name: "idNumber",
+            message: "What is your manager's Id number?",
+        },
+        {
+            type: "input",
+            name: "emailAddress",
+            message: "What is your manager's email address?"
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is your manager's office number?",
+        }
+    ]).then(results => {
+        const manager = new Manager(results.managerName, results.idNumber, results.emailAddress, results.officeNumber);
+        //push the new manager to an array
+        team.push(manager)
+        //run a createTeam function
+        createDevTeam();
+    })
+}
+
+const buildTeam = [
+    {
+        type: "list",
+        name: "teamMembers",
+        message: "Choose the next team member.",
+        choices: [
+            "Engineer",
+            "Intern",
+            "Employee",
+        ]
+    }
+]
+//in createDevTeam function, youll want to switch between creating an engineer, intern, or build out the team
+
+
+//create a function to make an engineer
+function createEngineer(){
+    console.log("Please type in your Engineer's information...");
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "engineer",
+            message: "What is your manager's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your manager's Id number?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your manager's email address?"
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is your manager's office number?"
         }
     ]).then(results => {
         const manager = new Manager(results.managerName, );
@@ -32,11 +91,6 @@ function createManager(){
         //run a createTeam function
     })
 }
-
-//in createDevTeam function, youll want to switch between creating an engineer, intern, or build out the team
-
-//create a function to make an engineer
-
 //create a function to make an intern
 
 //create a function to build out your team
